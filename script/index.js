0d02624abce98338587176df420fb50bde7414f8
marina <marina_bakum@mail.ru>
2017-12-14 08:05:34 +0200

    rename Del-> deleteTag, myFunction -> cloud, fix limitNew

--- script/index.js
+++ script/index.js
@@ -5,12 +5,11 @@ var tags = [
     ['blizzard', 400, 100]
 ]
 
-
 var n = 1
 
 for (var i = 0; i < tags.length; i++) {
     var el = document.createElement('li')
-    var del = '<i onclick="Del(' + n + ')">close</i>'
+    var del = '<i onclick="deleteTag(' + n + ')">close</i>'
     el.id = n++
     el.innerHTML = tags[i][0] + del
     el.style.left = tags[i][1] + 'px'
@@ -18,7 +17,7 @@ for (var i = 0; i < tags.length; i++) {
     tagCloud.insertBefore(el, tagCloud.childNodes[0])
 }
 
-function myFunction(el) {
+function cloud(el) {
     var elem = el.target || event.srcElement
     var limits = {
         top: 14,
@@ -49,7 +48,6 @@ function myFunction(el) {
                     document.getElementById(elem.id).querySelector('i').style.marginLeft = '10px'
                     document.getElementById(elem.id).querySelector('i').style.marginRight = '0'
                     elem.appendChild(document.getElementById(elem.id).querySelector('i'))
-
                 }
                 if (e.pageY > limits.bottom) {
                     newLocation.y = limits.bottom
@@ -59,24 +57,41 @@ function myFunction(el) {
                 elem.style.left = newLocation.x + 'px';
                 elem.style.top = newLocation.y - elem.offsetHeight / 2 + 'px';
             }
-
         }
-        delDisplayNone()
+        var delList = tagCloud.getElementsByTagName('i')
+        for (var i = 0; i < delList.length; i++) {
+            delList[i].style.display = 'none'
+        }
         document.onmouseup = function () {
             document.onmousemove = null
             document.onmouseup = null
-
         }
     }
     if (elem.id === 'tagCloud') {
         document.onmousemove = null
         document.onmouseup = null
         elem.ondblclick = function (e) {
+            var limitNew = {
+                left: 664  - 90,
+                top: 437  - 30,
+            }
             newTag.style.display = 'inline-block'
-            newTag.style.left = e.pageY + 'px'
-            newTag.style.top = e.pageX + 'px'
+            var newLeft = e.clientX - newTag.offsetHeight / 2
+            var newTop = e.clientY - newTag.offsetHeight / 2
+            if (limitNew.left < newLeft) {
+                newLeft -= 105
+            } else if (limitNew.left > newLeft){
+                newLeft += 10
+            }
+            if (limitNew.top < newTop) {
+                newTop -= 10
+            } else if (limitNew.top > newTop) {
+                newTop += 10
+            }
+            newTag.style.left = newLeft + 'px'
+            newTag.style.top = newTop + 'px'
             tagCloud.insertBefore(newTag, tagCloud.childNodes[0])
-            newTag.onchange = function (e) {
+            newTag.onchange = function () {
                 el = document.createElement('li')
                 del = '<i onclick="Del(' + n + ')">close</i>'
                 el.id = n++
@@ -88,18 +103,10 @@ function myFunction(el) {
                 newTag.value = ''
             }
         }
-
     }
 }
 
-function delDisplayNone() {
-    var delList = tagCloud.getElementsByTagName('i')
-    for (var i = 0; i < delList.length; i++) {
-        delList[i].style.display = 'none'
-    }
-}
-
-function Del(id) {
+function deleteTag(id) {
     var el = document.getElementById(id)
     el.parentNode.removeChild(el)
 }
